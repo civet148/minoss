0dc953ea64e0464c7a6751fc3c2991c428564d41
lory <civet148@outlook.com>
2024-01-24 11:08:02 +0800

    added: minio admin functions

--- go.mod
+++ go.mod
@@ -4,19 +4,41 @@ go 1.21.0
 
 require (
 	github.com/civet148/log v1.5.0
+	github.com/minio/madmin-go v1.7.5
 	github.com/minio/minio-go/v7 v7.0.66
 )
 
 require (
 	github.com/civet148/gotools v1.4.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fatih/color v1.12.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/google/uuid v1.5.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.17.4 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.6 // indirect
+	github.com/lufia/plan9stats v0.0.0-20220913051719-115f729f3c8c // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/minio/sha256-simd v1.0.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/philhofer/fwd v1.1.1 // indirect
+	github.com/power-devops/perfstat v0.0.0-20220216144756-c35f1ee13d7c // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/rs/xid v1.5.0 // indirect
+	github.com/secure-io/sio-go v0.3.1 // indirect
+	github.com/shirou/gopsutil/v3 v3.22.9 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/tinylib/msgp v1.1.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
+	github.com/tklauser/numcpus v0.5.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 )
--- minoss.go
+++ minoss.go
@@ -3,11 +3,16 @@ package minoss
 import (
 	"context"
 	"github.com/civet148/log"
+	"github.com/minio/madmin-go"
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 	"io"
+	"strings"
 )
 
+const (
+	oneMiB = 1024 * 1024
+)
 const (
 	OctetStream = "application/octet-stream"
 )
@@ -32,8 +37,31 @@ type Option struct {
 	Secure    bool   `json:"secure"`
 }
 
+type StorageUsage struct {
+	Used      uint64 `json:"used"`
+	Available uint64 `json:"available"`
+	Total     uint64 `json:"total"`
+}
+
+type StorageDetails struct {
+	Offline   uint64        `json:"offline"`
+	Available uint64        `json:"available"`
+	Online    uint64        `json:"online"`
+	Disks     []StorageDisk `json:"disks"`
+}
+
+type StorageDisk struct {
+	DeviceName        string `json:"device_name"`
+	DeviceSN          string `json:"device_sn"`
+	Instance          string `json:"instance"`
+	TotalCapacity     uint64 `json:"total_capacity"`
+	AvailableCapacity uint64 `json:"available_capacity"`
+	OK                bool   `json:"ok"`
+}
+
 type MinOSS struct {
 	mc  *minio.Client
+	ac  *madmin.AdminClient
 	opt Option
 }
 
@@ -49,26 +77,40 @@ func NewMinOSS(opt Option) *MinOSS {
 		log.Panic("create OSS client error [%s]", err.Error())
 		return nil
 	}
-
+	ac, err := madmin.New(opt.Endpoint, opt.AccessKey, opt.SecretKey, opt.Secure)
+	if err != nil {
+		log.Panic("create OSS admin error [%s]", err.Error())
+		return nil
+	}
 	s := &MinOSS{
 		mc:  mc,
+		ac:  ac,
 		opt: opt,
 	}
 	return s
 }
 
-func (m *MinOSS) Client() *minio.Client {
+// UserClient returns minio user client
+func (m *MinOSS) UserClient() *minio.Client {
 	return m.mc
 }
 
+// AdminClient returns minio admin client
+func (m *MinOSS) AdminClient() *madmin.AdminClient {
+	return m.ac
+}
+
+// Option return current OSS option
 func (m *MinOSS) Option() Option {
 	return m.opt
 }
 
-func (m *MinOSS) ListBucket(ctx context.Context) (buckets []minio.BucketInfo, err error) {
+// ListBuckets list all buckets
+func (m *MinOSS) ListBuckets(ctx context.Context) (buckets []minio.BucketInfo, err error) {
 	return m.mc.ListBuckets(ctx)
 }
 
+// MakeBucket create a bucket
 func (m *MinOSS) MakeBucket(ctx context.Context, bucket string) (err error) {
 	var exist bool
 	if bucket == "" {
@@ -90,6 +132,7 @@ func (m *MinOSS) MakeBucket(ctx context.Context, bucket string) (err error) {
 	return
 }
 
+// GetObjectList get bucket all objects
 func (m *MinOSS) GetObjectList(ctx context.Context, bucket string) (objects []*minio.ObjectInfo, total int64) {
 	objects = make([]*minio.ObjectInfo, 0)
 	objectCh := m.mc.ListObjects(ctx, bucket, minio.ListObjectsOptions{Recursive: false})
@@ -105,6 +148,7 @@ func (m *MinOSS) GetObjectList(ctx context.Context, bucket string) (objects []*m
 	return
 }
 
+// SearchObject search objects by bucket and file prefix
 func (m *MinOSS) SearchObject(ctx context.Context, bucket, prefix string, recursive bool) (objects []*minio.ObjectInfo, total int64) {
 	objects = make([]*minio.ObjectInfo, 0)
 	objectCh := m.mc.ListObjects(ctx, bucket, minio.ListObjectsOptions{Prefix: prefix, Recursive: recursive})
@@ -119,7 +163,8 @@ func (m *MinOSS) SearchObject(ctx context.Context, bucket, prefix string, recurs
 	return
 }
 
-func (m *MinOSS) UploadFile(ctx context.Context, bucket, objname string, object io.Reader, options ...minio.PutObjectOptions) (err error) {
+// UploadObject uploads an object to bucket
+func (m *MinOSS) UploadObject(ctx context.Context, bucket, objname string, object io.Reader, options ...minio.PutObjectOptions) (err error) {
 	var opts minio.PutObjectOptions
 	if len(options) == 0 {
 		opts = DefaultPutObjectOptions()
@@ -134,7 +179,9 @@ func (m *MinOSS) UploadFile(ctx context.Context, bucket, objname string, object 
 	return
 }
 
-func (m *MinOSS) DownloadFile(ctx context.Context, bucket, objname string, options ...minio.GetObjectOptions) (reader *minio.Object, err error) {
+// DownloadObject downloads an object
+func (m *MinOSS) DownloadObject(ctx context.Context, bucket, objname string, writer io.Writer, options ...minio.GetObjectOptions) (written int64, err error) {
+	var reader *minio.Object
 	var opts minio.GetObjectOptions
 	if len(options) == 0 {
 		opts = DefaultGetObjectOptions()
@@ -143,11 +190,12 @@ func (m *MinOSS) DownloadFile(ctx context.Context, bucket, objname string, optio
 	}
 	reader, err = m.mc.GetObject(ctx, bucket, objname, opts)
 	if err != nil {
-		return nil, log.Errorf("get object [%s] from bucket [%s] error [%s]", objname, bucket, err.Error())
+		return 0, log.Errorf("get object [%s] from bucket [%s] error [%s]", objname, bucket, err.Error())
 	}
-	return
+	return io.Copy(writer, reader)
 }
 
+// RemoveObject remove object from bucket
 func (m *MinOSS) RemoveObject(ctx context.Context, bucket, objname string, options ...minio.RemoveObjectOptions) (err error) {
 	var opts minio.RemoveObjectOptions
 	if len(options) == 0 {
@@ -158,6 +206,129 @@ func (m *MinOSS) RemoveObject(ctx context.Context, bucket, objname string, optio
 	err = m.mc.RemoveObject(ctx, bucket, objname, opts)
 	if err != nil {
 		return log.Error(err.Error())
+	}
+	return
+}
+
+// AdminGetQuota get bucket quota (MiB)
+func (m *MinOSS) AdminGetQuota(ctx context.Context, bucket string) (MiB float64, err error) {
+	var quota madmin.BucketQuota
+	quota, err = m.ac.GetBucketQuota(ctx, bucket)
+	if err != nil {
+		return 0, log.Errorf(err.Error())
+	}
+	return float64(quota.Quota / oneMiB), nil
+}
+
+// AdminSetQuota set bucket quota in hard mode (MiB)
+func (m *MinOSS) AdminSetQuota(ctx context.Context, bucket string, MiB int64) (err error) {
+	err = m.ac.SetBucketQuota(ctx, bucket, &madmin.BucketQuota{Quota: uint64(MiB * oneMiB), Type: madmin.HardQuota})
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+	return
+}
+
+// AdminGetBucketsUsage get all buckets usage information
+func (m *MinOSS) AdminGetBucketsUsage(ctx context.Context) (buckets map[string]madmin.BucketUsageInfo, err error) {
+	var usage madmin.DataUsageInfo
+	usage, err = m.ac.DataUsageInfo(ctx)
+	if err != nil {
+		log.Error(err.Error())
+		return
+	}
+	return usage.BucketsUsage, nil
+}
+
+// AdminGetDataUsage get total data usage information
+func (m *MinOSS) AdminGetDataUsage(ctx context.Context) (usage madmin.DataUsageInfo, err error) {
+	return m.ac.DataUsageInfo(ctx)
+}
+
+// AdminAddUser add user
+func (m *MinOSS) AdminAddUser(ctx context.Context, accessKey, secretKey string) (err error) {
+	return m.ac.AddUser(ctx, accessKey, secretKey)
+}
+
+// AdminRemoveUser delete user
+func (m *MinOSS) AdminRemoveUser(ctx context.Context, accessKey string) (err error) {
+	return m.ac.RemoveUser(ctx, accessKey)
+}
+
+// AdminSetPolicy set the policy by access key
+func (m *MinOSS) AdminSetPolicy(ctx context.Context, accessKey, policyName string, isGroup bool) (err error) {
+	return m.ac.SetPolicy(ctx, policyName, accessKey, isGroup)
+}
+
+// AdminSetUserStatus set user status (enabled or disabled)
+func (m *MinOSS) AdminSetUserStatus(ctx context.Context, accessKey string, status madmin.AccountStatus) (err error) {
+	return m.ac.SetUserStatus(ctx, accessKey, status)
+}
+
+// AdminGetServerInfo get server information
+func (m *MinOSS) AdminGetServerInfo(ctx context.Context) (info madmin.InfoMessage, err error) {
+	return m.ac.ServerInfo(ctx)
+}
+
+// AdminGetStorageInfo get storage information
+func (m *MinOSS) AdminGetStorageInfo(ctx context.Context) (info madmin.StorageInfo, err error) {
+	return m.ac.StorageInfo(ctx)
+}
+
+// AdminGetStorageUsage get storage usage information
+func (m *MinOSS) AdminGetStorageUsage(ctx context.Context) (usage StorageUsage, err error) {
+	var storage madmin.StorageInfo
+	storage, err = m.ac.StorageInfo(ctx)
+	if err != nil {
+		return StorageUsage{}, log.Error(err.Error())
+	}
+	for _, disk := range storage.Disks {
+		usage.Total += disk.TotalSpace
+		usage.Used += disk.UsedSpace
+		usage.Available += disk.AvailableSpace
+	}
+	return usage, nil
+}
+
+// AdminGetStorageDetails get all instance storage details
+func (m *MinOSS) AdminGetStorageDetails() (details map[string]StorageDetails, err error) {
+	ctx := context.Background()
+	var info madmin.InfoMessage
+	info, err = m.ac.ServerInfo(ctx)
+	if err != nil {
+		log.Error(err.Error())
+		return nil, err
+	}
+
+	details = make(map[string]StorageDetails)
+	for _, s := range info.Servers {
+		instance := strings.Split(s.Endpoint, ":")[0]
+		var online, offline, available uint64
+		sd := StorageDetails{}
+		sd.Disks = make([]StorageDisk, 0)
+		for _, disk := range s.Disks {
+			var ok bool
+			available += disk.AvailableSpace
+			if disk.State == "ok" {
+				online++
+				ok = true
+			} else {
+				offline++
+			}
+			sd.Disks = append(sd.Disks, StorageDisk{
+				DeviceName:        disk.DrivePath,
+				DeviceSN:          disk.UUID,
+				Instance:          s.Endpoint,
+				TotalCapacity:     disk.TotalSpace,
+				AvailableCapacity: disk.AvailableSpace,
+				OK:                ok,
+			})
+		}
+		sd.Offline = offline
+		sd.Online = online
+		sd.Available = available
+		details[instance] = sd
 	}
 	return
 }
